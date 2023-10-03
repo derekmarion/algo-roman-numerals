a121a9a330c57cd1cd33e1748b8aa0e3ea6429a6
Derek Marion <derek.j.marion@gmail.com>
2023-10-03 15:35:39 -0700

    Got toRoman function working

--- romanNumerals.js
+++ romanNumerals.js
@@ -25,7 +25,34 @@ function toRomanLazy(num) {
 }
 
 function toRoman(num) {
-  return "";
+  let output = ""
+  const romanNumeralToArabic = {
+    "I": 1,
+    "IV": 4,
+    "V": 5,
+    "IX": 9,
+    "X": 10,
+    "XL": 40,
+    "L": 50,
+    "C": 100,
+    "CD": 400,
+    "D": 500,
+    "CM": 900,
+    "M": 1000
+  }
+  const romanNumeralPriorityOrder = ['M', 'CM', 'D', 'CD', 'C', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I']
+  for (let numeral of romanNumeralPriorityOrder) {
+    let quantity = Math.floor(num / romanNumeralToArabic[numeral]);
+    if (quantity > 0) {
+      for (i = quantity; i > 0; i--) { 
+        output += numeral;
+        num -= romanNumeralToArabic[numeral]; 
+      };
+      if (num === 0) {
+        return output;
+      }; 
+    };
+  }
 }
 
 module.exports = { toRoman, toRomanLazy };
