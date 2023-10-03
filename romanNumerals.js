01b0878bfabeecdb9886fa0fe1aff3cb01d8aba4
Derek Marion <derek.j.marion@gmail.com>
2023-10-03 15:19:08 -0700

    First working version of toRomanLazy function

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,5 +1,27 @@
 function toRomanLazy(num) {
-  return "";
+  let output = ""
+  const romanNumeralToArabic = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+  }
+  const romanNumeralPriorityOrder = ['M', 'D', 'C', 'L', 'X', 'V', 'I']
+  for (let numeral of romanNumeralPriorityOrder) {
+    let quantity = Math.floor(num / romanNumeralToArabic[numeral]);
+    if (quantity > 0) {
+      for (i = quantity; i > 0; i--) { 
+        output += numeral;
+        num -= romanNumeralToArabic[numeral]; 
+      };
+      if (num === 0) {
+        return output;
+      }; 
+    };
+  }
 }
 
 function toRoman(num) {
