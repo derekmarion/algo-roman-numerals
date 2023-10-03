e74383583583ba97d5c36e176b3379bbcfec52ef
Derek Marion <derek.j.marion@gmail.com>
2023-10-03 15:20:02 -0700

    Corrected error in test case output for toRomanLazy function

--- romanNumeralsSpec.js
+++ romanNumeralsSpec.js
@@ -2,7 +2,7 @@ const { toRomanLazy, toRoman } = require("./romanNumerals.js");
 
 console.log(toRomanLazy(4) === "IIII");
 console.log(toRomanLazy(150) === "CL");
-console.log(toRomanLazy(944) === "DCCCCXXXXIIII'");
+console.log(toRomanLazy(944) === "DCCCCXXXXIIII");
 
 console.log(toRoman(4) === "IV");
 console.log(toRoman(150) === "CL");
